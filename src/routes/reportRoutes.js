7f98cde907efdaeadbf3aa9ebad14371e1ccc6e3
HASAN-SHAIK <59140951+HASAN-SHAIK@users.noreply.github.com>
2026-08-18 16:34:02 +0530

    Route V1 sales report through immutable product snapshots

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getSalesReport, getInventoryReport, getProfitReport, getProfitGraph } = require('../controllers/reportController');
+const { getInventoryReport, getProfitReport, getProfitGraph } = require('../controllers/reportController');
+const { getHistoricalSalesReport } = require('../controllers/historicalSalesReportController');
 const { getBranchInventoryReport } = require('../controllers/branchInventoryReportController');
 const { getDailySalesReport } = require('../controllers/dailySalesReportController');
 const { getCustomerOutstandingReport } = require('../controllers/customerOutstandingReportController');
@@ -13,7 +14,7 @@ const getInventoryReportForScope = (req, res) => {
   return getInventoryReport(req, res);
 };
 
-router.get('/sales', requirePermission('reports:read'), requireReportScope, requireReportDateRange, getSalesReport);
+router.get('/sales', requirePermission('reports:read'), requireReportScope, requireReportDateRange, getHistoricalSalesReport);
 router.get('/inventory', requirePermission('reports:read'), requireReportScope, getInventoryReportForScope);
 router.get('/daily', requirePermission('reports:read'), requireReportScope, getDailySalesReport);
 router.get('/profit', requirePermission('reports:read'), requireReportScope, requireReportDateRange, getProfitReport);
